8cc5a4b384c7d3f8669e1ecc8b6e6bbb2e63f1ba
nothendev <borodinov.ilya@gmail.com>
2023-07-09 16:33:36 +0300

    fixup

---
+++ devenv.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+
+{
+  scripts.stage.exec = "git add .; git commit -m '[devenv] auto-commit'";
+  scripts.push.exec = "stage; git commit --amend -m $1; git push nixos";
+
+  pre-commit.hooks.nixfmt.enable = true;
+}
--- home/init.nix
+++ home/init.nix
@@ -1,7 +1,8 @@
 { pkgs, osConfig, ... }:
 
 {
-  imports = [ ./neofetch.nix ./pretty.nix ./configs.nix ./packages.nix ];
+  imports =
+    [ ./neofetch.nix ./pretty.nix ./configs.nix ./packages.nix ./fish.nix ];
   home.username = "ilya";
   home.homeDirectory = "/home/ilya";
   home.stateVersion = osConfig.system.stateVersion;
