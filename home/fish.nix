5fffd0e9c03219432fcabfb5fe8346167678592a
nothendev <borodinov.ilya@gmail.com>
2023-07-09 16:38:16 +0300

    [justfile] stage objects

--- home/fish.nix
+++ home/fish.nix
@@ -1,11 +1,9 @@
-{ config, pkgs, ... }:
-{
+{ config, pkgs, ... }: {
   programs.fish.enable = true;
-  programs.fish.functions = [
-    ''
-      function l
-      exa -1amFhl --icons $argv
-      end
-    ''
-  ];
+  programs.fish.functions = [''
+    function l
+    exa -1amFhl --icons $argv
+    end
+  ''];
+  programs.direnv.enable = true;
 }
