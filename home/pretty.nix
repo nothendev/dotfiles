f98b4e1a1353c2e5c85d99d4774d930bba8efad2
nothendev <borodinov.ilya@gmail.com>
2023-07-09 15:21:04 +0300

    hhhhh

--- home/pretty.nix
+++ home/pretty.nix
@@ -1,12 +1,16 @@
-{ config, pkgs, ... }:
+{ config, pkgs, osConfig, ... }:
 {
   programs.kitty = {
     enable = true;
     package = pkgs.kitty;
     font = {
-      package = pkgs.monocraft;
-      name = "Monocraft Nerd Font";
+      name = "JetBrainsMono NF Regular";
       size = 14;
     };
+    shellIntegration.enableFishIntegration = true;
+    extraConfig = ''
+      background_opacity 0.5
+      disable_ligatures never
+    '';
   };
 }
