6fefdbcf68d2785f2c5734501a0cfc5a307d2f87
nothendev <borodinov.ilya@gmail.com>
2023-07-09 16:34:04 +0300

    fixup

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,7 @@
 
 {
   scripts.stage.exec = "git add .; git commit -m '[devenv] auto-commit'";
-  scripts.push.exec = "stage; git commit --amend -m $1; git push nixos";
+  scripts.push.exec = "stage; git commit --amend -m $1; git push gh";
 
   pre-commit.hooks.nixfmt.enable = true;
 }
