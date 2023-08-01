9a78afaa9c0223ed6667b195a44093f8dff2fc6c
nothendev <borodinov.ilya@gmail.com>
2023-08-01 15:11:08 +0300

    [justfile] stage objects

--- overlays/zig-nightly.nix
+++ overlays/zig-nightly.nix
@@ -1,6 +1,6 @@
 final: prev: {
   zig = prev.zig.overrideAttrs (old: {
-    src = prev.fetchFromGithub {
+    src = prev.fetchFromGitHub {
       owner = "ziglang";
       repo = "zig";
       rev = "4f6013bf50e1b99b5c9ea238a2165bfe41fc57d4";
@@ -8,7 +8,7 @@ final: prev: {
     };
   });
   zls = prev.zls.overrideAttrs (old: {
-    src = prev.fetchFromGithub {
+    src = prev.fetchFromGitHub {
       owner = "zigtools";
       repo = "zls";
       rev = "f71c42b42d15a02b515806ab4226868180e33b67";
