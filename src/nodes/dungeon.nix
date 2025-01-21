6d7aec45e9afa99d5b8b89ad0c7067ee79c6535a
Borodinov Ilya <borodinov.ilya@gmail.com>
2025-01-21 23:39:01 +0300

    I am morally and utterly screwed

--- src/nodes/dungeon.nix
+++ src/nodes/dungeon.nix
@@ -55,6 +55,7 @@
       plugins = [
         "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e"
       ];
+      hash = "sha256-JoujVXRXjKUam1Ej3/zKVvF0nX97dUizmISjy3M3Kr8=";
     };
     enableReload = true;
     virtualHosts = {
@@ -68,7 +69,7 @@
       "project.minkystudios.ru" = {
         extraConfig = ''
           root * /srv/project
-          encode zstd gzip br
+          encode zstd gzip
           file_server
         '';
       };
