5a37bab59f3f94afc7a1c2a57e63b57e2fa8b2f9
Borodinov Ilya <borodinov.ilya@gmail.com>
2025-08-19 22:03:42 +0300

    feat(nvim): various improvements

--- configs/nvim.lua
+++ configs/nvim.lua
@@ -14,7 +14,8 @@ opts.o = {
   tabstop = 2,
   expandtab = true,
   signcolumn = "yes",
-  winborder = "rounded"
+  winborder = "rounded",
+  swapfile = false
 }
 
 for namespace, options in pairs(opts) do
@@ -24,10 +25,11 @@ for namespace, options in pairs(opts) do
 end
 
 vim.keymap.set("n", "<leader>o", ":update<CR>:source<CR>")
-vim.keymap.set({ "n", "i", "v", "x" }, "<C-a>", "0", { noremap = true })
+-- vim.keymap.set({ "n", "i", "v", "x" }, "<C-a>", "0", { noremap = true })
 vim.keymap.set({ "n", "i", "v", "x" }, "<C-c>", "<Esc>", { noremap = true })
 
 vim.pack.add({
+  -- "https://github.com/glacambre/firenvim",
   "https://github.com/stevearc/oil.nvim",
   "https://github.com/echasnovski/mini.pick",
   "https://github.com/echasnovski/mini.icons",
@@ -35,13 +37,19 @@ vim.pack.add({
   "https://github.com/vague2k/vague.nvim",
   "https://github.com/rafamadriz/friendly-snippets",
   "https://github.com/supermaven-inc/supermaven-nvim",
+  "https://github.com/Saecki/crates.nvim",
   "https://github.com/saghen/blink.compat",
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter",
+    version = "main"
+  },
   {
     src = "https://github.com/saghen/blink.cmp",
     version = vim.version.range('1.*'),
   }
 })
 
+require "crates".setup()
 require "mini.pick".setup()
 require "oil".setup()
 require "vague".setup {
@@ -68,42 +76,20 @@ require "blink.cmp".setup {
     },
     menu = {
       max_height = 20,
-      draw = {
-        components = {
-          kind_icon = {
-            ellipsis = false,
-            text = function(ctx)
-              if ctx.source_name == "supermaven" then
-                return "󰚩"
-              end
-
-              local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-              return kind_icon
-            end,
-            highlight = function(ctx)
-              local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-              return hl
-            end,
-          },
-        },
-      },
     },
   },
   keymap = {
-    preset = "none",
+    preset = "enter",
     ["<C-Space>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
     ["<Tab>"] = { "select_next", "fallback" },
     ["<S-Tab>"] = { "select_prev", "fallback" },
     ["<CR>"] = {
-      function(cmp)
-        cmp.hide()
-        cmp.accept()
-      end,
+      "accept",
       "fallback",
     },
   },
   sources = {
-    default = { "supermaven", "lsp", "path", "snippets", "buffer" },
+    default = { "supermaven", "lsp", "path", "snippets" },
     providers = {
       supermaven = {
         name = "supermaven",
@@ -114,16 +100,58 @@ require "blink.cmp".setup {
   },
 }
 
+-- require"nvim-treesitter".install{'rust','lua'}
+
+-- vim.api.nvim_create_autocmd('FileType', {
+--   pattern = { 'rust', 'lua' },
+--   callback = function()
+--     vim.treesitter.start()
+--   end,
+-- })
+
 vim.keymap.set("n", "<leader>ff", ":Pick files tool='fd'<CR>")
 vim.keymap.set("n", "<leader>fw", ":Pick grep_live tool='rg'<CR>")
 vim.keymap.set("n", "<leader>fh", ":Pick help<CR>")
 vim.keymap.set("n", "<leader>O", ":Oil<CR>")
+vim.keymap.set("n", "<leader>C", "1z=")
 vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 
 vim.lsp.enable({
   "lua_ls",
-  "rust-analyzer"
+  "rust_analyzer"
+})
+
+vim.lsp.config("rust_analyzer", {
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        --extraEnv = {
+        --  RUSTFLAGS = "-Clink-arg=-fuse-ld=mold",
+        --},
+        buildScripts = { enable = true },
+        features = "all",
+        targetDir = true,
+      },
+      diagnostics = {
+        disabled = { "proc-macro-disabled", "proc-macros-disabled" },
+      },
+      lens = {
+        enable = true,
+      },
+      procMacro = {
+        ignored = {
+          ["leptos_macro"] = { "component", "server", "island" },
+          ["vespid_macros"] = { "component" },
+        },
+      },
+      checkOnSave = false,
+    },
+  }
 })
 
 vim.cmd("colorscheme vague")
 vim.cmd("hi statusline guibg=bg")
+-- vim.cmd("call firenvim#install(0)")
